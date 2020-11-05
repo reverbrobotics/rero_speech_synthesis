2beb996fa909b76494ba32fbdac9543ffcc0b627
lukeinator42 <lukasgrasse@gmail.com>
2020-11-05 14:14:24 -0700

    added parameters

--- src/speech_synthesis.py
+++ src/speech_synthesis.py
@@ -14,14 +14,26 @@ rospy.init_node('ros_tts_node', anonymous=True)
 tts_topic_name = rospy.get_param('~topic_name')
 rospy.loginfo("%s is %s", rospy.resolve_name('tts_topic_name'), tts_topic_name)
 
+pitch = rospy.get_param('pitch')
+rospy.loginfo("%s is %s", rospy.resolve_name('pitch'), pitch)
+
+speed = rospy.get_param('speed')
+rospy.loginfo("%s is %s", rospy.resolve_name('speed'), speed)
+
+voice = rospy.get_param('voice')
+rospy.loginfo("%s is %s", rospy.resolve_name('voice'), voice)
+
+loop_rate = rospy.get_param('loop_rate')
+rospy.loginfo("%s is %s", rospy.resolve_name('loop_rate'), loop_rate)
+
 loop_rate = rospy.get_param('loop_rate')
 rospy.loginfo("%s is %s", rospy.resolve_name('loop_rate'), loop_rate)
 
 rate = rospy.Rate(loop_rate)  # 10hz
 
-esng = ESpeakNG(voice='en')
-esng.pitch = 32
-esng.speed = 150
+esng = ESpeakNG(voice=voice)
+esng.pitch = pitch
+esng.speed = speed
 
 def callback(data):
 	print(data)
